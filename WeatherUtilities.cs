99f8980fccba5f0118c59e274ff549e3a86893e6
Michael Robards <mikerobards@icloud.com>
2021-09-19 13:39:51 -0400

    correct error in calcs

--- WeatherUtilities.cs
+++ WeatherUtilities.cs
@@ -6,12 +6,12 @@ namespace MyUtilities
     {
         static public float FahrenheitToCelsius(float temperatureFahrenheit)
         {
-            return temperatureFahrenheit - 32 / 1.8f;
+            return (temperatureFahrenheit - 32) / 1.8f;
         }
 
         static public float CelsiusToFahrenheit(float temperatureCelsius)
         {
-            return temperatureCelsius * 1.8f + 32;
+            return (temperatureCelsius * 1.8f) + 32;
         }
 
         // The higher the index, the lower the comfort
