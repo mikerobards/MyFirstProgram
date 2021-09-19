487548557731491f4d9f8c89b492b3228f2632cd
Michael Robards <mikerobards@icloud.com>
2021-09-19 13:12:24 -0400

    make methods public

--- Program.cs
+++ Program.cs
@@ -1,40 +1,41 @@
 ﻿using System;
 using System.Globalization;
+using MyUtilities;
 
 namespace MyFirstProgram
 {
-    // class Program
-    // {
-    //     static void Main(string[] args)
-    //     {
-    //         Console.WriteLine("Hello World!");
-    //     }
-
-    // }
-    class Temperature
+    class CheckComfort
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Enter Farenheit Temperature:");
-            string temperatureFarenheitString = Console.ReadLine();
-            decimal temperatureFarenheit = decimal.Parse(temperatureFarenheitString, CultureInfo.InvariantCulture.NumberFormat);
-            Console.WriteLine(Temperature.FarenheitToCelsius(temperatureFarenheit));
+            WeatherUtilities.Report("Kennesaw", 23, 65); // ~73F
+        }
 
-        }
-        static decimal FarenheitToCelsius(decimal temperatureFarenheit)
-        {
-            decimal temperatureCelsius = (temperatureFarenheit - 32M) / 1.8M;
-            int result = (int)temperatureCelsius;
-            // return result;
-            Console.WriteLine(temperatureCelsius);
-            if (temperatureCelsius - result >= 0.5M)
-            {
-                return Math.Ceiling(temperatureCelsius);
-            }
-            else
-            {
-                return Math.Floor(temperatureCelsius);
-            }
-        }
     }
+    // class Temperature
+    // {
+    //     static void Main(string[] args)
+    //     {
+    //         Console.WriteLine("Enter Farenheit Temperature:");
+    //         string temperatureFarenheitString = Console.ReadLine();
+    //         decimal temperatureFarenheit = decimal.Parse(temperatureFarenheitString, CultureInfo.InvariantCulture.NumberFormat);
+    //         Console.WriteLine(Temperature.FarenheitToCelsius(temperatureFarenheit));
+
+    //     }
+    //     static decimal FarenheitToCelsius(decimal temperatureFarenheit)
+    //     {
+    //         decimal temperatureCelsius = (temperatureFarenheit - 32M) / 1.8M;
+    //         int result = (int)temperatureCelsius;
+    //         // return result;
+    //         Console.WriteLine(temperatureCelsius);
+    //         if (temperatureCelsius - result >= 0.5M)
+    //         {
+    //             return Math.Ceiling(temperatureCelsius);
+    //         }
+    //         else
+    //         {
+    //             return Math.Floor(temperatureCelsius);
+    //         }
+    //     }
+}
 }
--- WeatherUtilities.cs
+++ WeatherUtilities.cs
@@ -4,28 +4,27 @@ namespace MyUtilities
 {
     class WeatherUtilities
     {
-        static float FahrenheitToCelsius(float temperatureFahrenheit)
+        static public float FahrenheitToCelsius(float temperatureFahrenheit)
         {
-            var temperatureCelsius = temperatureFahrenheit - 32 / 1.8f;
-            return temperatureCelsius;
+            return temperatureFahrenheit - 32 / 1.8f;
         }
 
-        static float CelsiusToFahrenheit(float temperatureCelsius)
+        static public float CelsiusToFahrenheit(float temperatureCelsius)
         {
-            var temperatureFahrenheit = temperatureCelsius * 1.8f + 32;
-            return temperatureFahrenheit;
+            return temperatureCelsius * 1.8f + 32;
         }
 
-        static float ComfortIndex(float temperatureFahrenheit, float humidityPercent)
+        // The higher the index, the lower the comfort
+        static public float ComfortIndex(float temperatureFahrenheit, float humidityPercent)
         {
-            var comfortIndex = (temperatureFahrenheit + humidityPercent) / 4;
-            return comfortIndex;
+            // Probably not a very reliable formula:
+            return (temperatureFahrenheit + humidityPercent) / 4; ;
         }
 
-        static void Report(string location, float temperatureCelsius, float humidity)
+        static public void Report(string location, float temperatureCelsius, float humidity)
         {
             var temperatureFahrenheit = CelsiusToFahrenheit(temperatureCelsius);
-            Console.WriteLine("Comfort Index for " + location + ": " + ComfortIndex(temperatureFahrenheit, humidity));
+            Console.WriteLine($"Comfort Index for {location}: {ComfortIndex(temperatureFahrenheit, humidity)}");
         }
     }
 }
