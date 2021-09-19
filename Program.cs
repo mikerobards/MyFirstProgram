12ea3b953b21e917890d331f23577b9b1ba3c585
Michael Robards <mikerobards@icloud.com>
2021-09-19 13:19:38 -0400

    add other cities

--- Program.cs
+++ Program.cs
@@ -8,6 +8,9 @@ namespace MyFirstProgram
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Where should we go in May?");
+            WeatherUtilities.Report("San Francisco", WeatherUtilities.FahrenheitToCelsius(65), 73);
+            WeatherUtilities.Report("Denver", WeatherUtilities.FahrenheitToCelsius(77), 55);
             WeatherUtilities.Report("Kennesaw", 23, 65); // ~73F
         }
 
